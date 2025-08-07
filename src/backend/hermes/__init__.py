25ecef9ce31ea9f2de54cbd5ba231fa509ee074a
Hongyu Shi <shywzt@iCloud.com>
2025-08-07 10:34:50 +0800

    refactor(hermes): reorganize code
    Signed-off-by: Hongyu Shi <shywzt@iCloud.com>

--- src/backend/hermes/__init__.py
+++ src/backend/hermes/__init__.py
@@ -1,23 +1,27 @@
 """Hermes Chat API 模块"""
 
-from .client import (
-    HermesAPIError,
-    HermesApp,
-    HermesChatClient,
-    HermesChatRequest,
-    HermesFeatures,
-    HermesMessage,
-    HermesStreamEvent,
-    validate_url,
-)
+from .client import HermesChatClient, validate_url
+from .exceptions import HermesAPIError
+from .models import HermesAgent, HermesApp, HermesChatRequest, HermesFeatures, HermesMessage
+from .services.agent import HermesAgentManager
+from .services.conversation import HermesConversationManager
+from .services.http import HermesHttpManager
+from .services.model import HermesModelManager
+from .stream import HermesStreamEvent, HermesStreamProcessor
 
 __all__ = [
     "HermesAPIError",
+    "HermesAgent",
+    "HermesAgentManager",
     "HermesApp",
     "HermesChatClient",
     "HermesChatRequest",
+    "HermesConversationManager",
     "HermesFeatures",
+    "HermesHttpManager",
     "HermesMessage",
+    "HermesModelManager",
     "HermesStreamEvent",
+    "HermesStreamProcessor",
     "validate_url",
 ]
